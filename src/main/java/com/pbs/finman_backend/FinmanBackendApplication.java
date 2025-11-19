544073ebeb611185f3006113d6dee9f1f1435c62
Puneeth-Babu <puneeth.babu@wissen.com>
2025-11-19 13:26:28 +0530

    Initial commit

---
+++ src/main/java/com/pbs/finman_backend/FinmanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pbs.finman_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinmanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbs/finman_backend/FinmanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.pbs.finman_backend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FinmanBackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
