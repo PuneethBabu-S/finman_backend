87b43f5397ae7443bd5e7e00904f891003f59f0b
Puneeth-Babu <puneeth.babu@wissen.com>
2025-11-22 23:21:44 +0530

    Added swagger config

--- src/main/java/com/pbs/finman_backend/security/SecurityConfig.java
+++ src/main/java/com/pbs/finman_backend/security/SecurityConfig.java
@@ -26,7 +26,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                            "/api/auth/**",
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
